1b1108eabf0e2c5186857c27f0f6facd7d64978d
VGroupPablo <129444988+VGroupPablo@users.noreply.github.com>
2023-03-31 17:26:20 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,29 +1,26 @@
 Citizen.CreateThread(function()
 	local shake_counter1 = 0
 	local shake_counter2 = 0
+	local PlayerId = PlayerId(-1)
+	local PlayerPed = PlayerPedId(-1)
 
 	while true do
 		Citizen.Wait(0)
 	
-		if IsPlayerFreeAiming(PlayerId()) and IsPedStill(GetPlayerPed(-1)) and not IsPedAimingFromCover(GetPlayerPed(-1)) then
+		if IsPlayerFreeAiming(PlayerId) and IsPedStill(PlayerPed) and not IsPedAimingFromCover(PlayerPed) then
 			Citizen.Wait(100)
 			shake_counter1 = shake_counter1 + 1
 			shake_counter2 = 0
 			if (shake_counter1 == 5) then 
 				ShakeGameplayCam('HAND_SHAKE', Config.shakestill) 
 			end
-		elseif IsPlayerFreeAiming(PlayerId()) and not IsPedStill(GetPlayerPed(-1)) then
+		elseif IsPlayerFreeAiming(PlayerId) and not IsPedStill(PlayerPed) then
 			Citizen.Wait(10)
 			shake_counter2 = shake_counter2 + 1
 			shake_counter1 = 0
 			if (shake_counter2 == 5) then 
 				ShakeGameplayCam('HAND_SHAKE', Config.shakemovement) 
 			end
-		elseif IsPedAimingFromCover(GetPlayerPed(-1)) then
-			StopGameplayCamShaking(true)
-			shake_counter1 = 0
-			shake_counter2 = 0
-		else
 			shake_counter1 = 0
 			shake_counter2 = 0
 			StopGameplayCamShaking(true)
